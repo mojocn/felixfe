1733b6274bbd1ab4f964dbec8462d006212255ae
EricZhou <1413507308@qq.com>
2019-05-08 15:14:04 +0800

    optimize

--- src/libs/request.js
+++ src/libs/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {Message,MessageBox}from 'element-ui'
+import {Message, MessageBox} from 'element-ui'
 import vueRouter from "../router";
 
 // function getBaseUrl() {
@@ -21,7 +21,7 @@ const service = axios.create({
     },
     // 修改请求数据,去除data.q中为空的数据项,只针对post请求
 
-})
+});
 
 service.interceptors.request.use(config => {
 
@@ -31,21 +31,19 @@ service.interceptors.request.use(config => {
     }
     let expire_ts = localStorage.getItem('expire_ts');
     if (expire_ts && parseInt(expire_ts) < (Date.now()/1000)){
-        Message.error('token has been expired')
-        return Promise.reject(null)
+        return Promise.reject({code: 1, msg: "token has been expired", title: "Token Expire"})
     }
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem('token');
     if (!token){
-        Message.error('has no token jump to login page')
-        return Promise.reject(null)
+        return Promise.reject({code: 1, msg: "felix has no token jump to login page", title: "No Token"})
     }
-    config.headers['Authorization'] = 'Bearer ' + token // 让每个请求携带自定义token 请根据实际情况自行修改
+    config.headers['Authorization'] = 'Bearer ' + token; // 让每个请求携带自定义token 请根据实际情况自行修改
 
 
     return config;
 }, error => {
     // 对请求错误做些什么
-    Message.error("sending request is failed")
+    Message.error("sending request is failed");
     // let loading = Loading.service({target:'#felix'});
     // loading.close();    //关闭加载前，记得重新定义实例
     return Promise.reject(error);
@@ -53,25 +51,34 @@ service.interceptors.request.use(config => {
 
 // http response 拦截器
 service.interceptors.response.use(response => {
-        let data = response.data
+        let data = response.data;
         if (data.ok){
             return data;
         }else {
-            Message.error(data.msg)
+            Message.error(data.msg);
             return {};
         }
     },
     error => {
-
+        if (error.code === 1) {
+            MessageBox.confirm(error.msg, error.title, {
+                confirmButtonText: 'OK',
+                cancelButtonText: 'Cancel',
+                type: 'warning'
+            }).then(() => {
+                vueRouter.push({name: "login"})
+            });
+            return Promise.reject(null)
+        }
         if (error.response && error.response.status === 412){
 
-            MessageBox.confirm('未被授权，请重新登录', '重新登录', {
-                confirmButtonText: '重新登录',
-                cancelButtonText: '取消',
+            MessageBox.confirm('token is invalid', 'jump to login', {
+                confirmButtonText: 'OK',
+                cancelButtonText: 'Cancel',
                 type: 'warning'
             }).then(() => {
                 vueRouter.push({name:"login"})
-            })
+            });
             return Promise.reject(null)
         }
 
