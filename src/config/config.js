674a27a83e2b81e46416cda109c3af68a1b5cee6
EricZhou <dejavuzhou@qq.com>
2019-07-31 18:50:36 +0800

    fix ws

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,7 @@ let config = {};
 
 config.wsBase = `${window.location.protocol === "https:" ? "wss://" : "ws://"}${window.location.host}`;
 config.apiBase = `${window.location.protocol}//:${window.location.host}`;
-if (window.location.host.includes('felix.mojotv.cn')) {
-    //to fix nginx web socket upgrade error
-    config.wsBase = "ws://felix.mojotv.cn:2222"
-}
+
 if (window.location.host.includes('127.0.0.1')) {
     //to fix nginx web socket upgrade error
     config.wsBase = "ws://127.0.0.1:2222"
