000052c0f251250290f59f6c278facbf87f25957
mojocn <neochau@gmail.com>
2024-11-15 10:17:42 +0800

    add cloudflare function users

---
+++ functions/api/users.ts
@@ -0,0 +1,11 @@
+import { D1Database,PagesFunction,Env } from "@cloudflare/workers-types";
+
+
+interface Env {
+	DB: D1Database;
+}
+
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+	const users = await context.env.DB.prepare("SELECT * FROM users").all();
+	return new Response(users);
+};
